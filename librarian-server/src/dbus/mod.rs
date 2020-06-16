36d8273439bca4e99d02df1650c43cb8f08b22f6
Christian Poveda <git@christianpoveda.xyz>
2020-06-15 19:44:19 -0500

    use anyhow inside dbus module

--- librarian-server/src/dbus/mod.rs
+++ librarian-server/src/dbus/mod.rs
@@ -3,7 +3,7 @@ use anyhow::{anyhow, Result};
 use dbus::{
     arg::{RefArg, Variant},
     blocking::LocalConnection,
-    tree::{Factory, MethodErr},
+    tree::Factory,
 };
 
 use std::collections::HashMap;
@@ -15,8 +15,8 @@ use crate::library::Library;
 mod cli;
 mod search;
 
-use search::OrgGnomeShellSearchProvider2;
 use cli::LbrCli;
+use search::OrgGnomeShellSearchProvider2;
 
 pub(crate) fn serve(lib: Library) -> Result<()> {
     let mut conn = LocalConnection::new_session()?;
@@ -73,32 +73,34 @@ impl LibrarySync {
 }
 
 impl OrgGnomeShellSearchProvider2 for LibrarySync {
-    fn get_initial_result_set(&self, terms: Vec<&str>) -> Result<Vec<String>, MethodErr> {
-        let lib = self.read().map_err(|e| MethodErr::failed(&e))?;
-
+    fn get_initial_result_set(&self, terms: Vec<&str>) -> Result<Vec<String>> {
         let query = terms.join(" ");
-        Ok(lib.search(&query).map(|id| id.to_string()).collect())
+        Ok(self
+            .read()?
+            .search(&query)
+            .map(|id| id.to_string())
+            .collect())
     }
 
     fn get_subsearch_result_set(
         &self,
         _previous_results: Vec<&str>,
         terms: Vec<&str>,
-    ) -> Result<Vec<String>, MethodErr> {
+    ) -> Result<Vec<String>> {
         self.get_initial_result_set(terms)
     }
 
     fn get_result_metas(
         &self,
         identifiers: Vec<&str>,
-    ) -> Result<Vec<HashMap<String, Variant<Box<dyn RefArg + 'static>>>>, MethodErr> {
-        let lib = self.read().map_err(|e| MethodErr::failed(&e))?;
+    ) -> Result<Vec<HashMap<String, Variant<Box<dyn RefArg + 'static>>>>> {
+        let lib = self.read()?;
 
         let mut metas = Vec::default();
 
         for identifier in identifiers {
-            let id = identifier.parse().map_err(|e| MethodErr::failed(&e))?;
-            let doc = lib.get(id).map_err(|e| MethodErr::failed(&e))?;
+            let id = identifier.parse()?;
+            let doc = lib.get(id)?;
 
             let id: Variant<Box<dyn RefArg + 'static>> = Variant(Box::new(id.to_string()));
             let name: Variant<Box<dyn RefArg + 'static>> = Variant(Box::new(doc.title.clone()));
@@ -116,19 +118,11 @@ impl OrgGnomeShellSearchProvider2 for LibrarySync {
         Ok(metas)
     }
 
-    fn activate_result(
-        &self,
-        identifier: &str,
-        _terms: Vec<&str>,
-        _timestamp: u32,
-    ) -> Result<(), MethodErr> {
-        let lib = self.read().map_err(|e| MethodErr::failed(&e))?;
-
-        let id = identifier.parse().map_err(|e| MethodErr::failed(&e))?;
-
-        lib.open(id).map_err(|e| MethodErr::failed(&e))
+    fn activate_result(&self, identifier: &str, _terms: Vec<&str>, _timestamp: u32) -> Result<()> {
+        let id = identifier.parse()?;
+        self.read()?.open(id)
     }
-    fn launch_search(&self, _terms: Vec<&str>, _timestamp: u32) -> Result<(), MethodErr> {
+    fn launch_search(&self, _terms: Vec<&str>, _timestamp: u32) -> Result<()> {
         Ok(())
     }
 }
@@ -140,12 +134,9 @@ impl AsRef<dyn OrgGnomeShellSearchProvider2 + 'static> for LibrarySync {
 }
 
 impl LbrCli for LibrarySync {
-    fn insert(&self, doc: &str, path: &str) -> Result<(), MethodErr> {
-        let mut lib = self.write().map_err(|e| MethodErr::failed(&e))?;
-
-        let doc = serde_json::from_str(doc).map_err(|e| MethodErr::failed(&e))?;
-
-        lib.insert(doc, path).map_err(|e| MethodErr::failed(&e))
+    fn insert(&self, doc: &str, path: &str) -> Result<()> {
+        let doc = serde_json::from_str(doc)?;
+        self.write()?.insert(doc, path)
     }
 }
 
